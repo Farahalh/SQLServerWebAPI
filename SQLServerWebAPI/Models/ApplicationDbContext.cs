007268a69c9cc190e9ad14c3a0da3f582c96d54d
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-30 08:21:47 +0100

    Creating class ApplicationDbContext that inherits from DbContext and contains theOnConfiguring method.

---
+++ SQLServerWebAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace SQLServerWebAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=ITHS-HP-EB-059\SQLEXPRESS;Database=MyDb;Trusted_Connection=True;TrustServerCertificate=True;")
+                        .LogTo(message => Debug.WriteLine(message))
+                        .EnableSensitiveDataLogging();
+        }
+    }
+}
