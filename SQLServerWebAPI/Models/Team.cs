2b4c9d52b93fd2bd59a023ebc0f3f27a0e2b85b5
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-30 08:19:43 +0100

    Creating class Team

---
+++ SQLServerWebAPI/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace SQLServerWebAPI.Models
+{
+    public class Team
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public int? FoundedYear { get; set; }
+    }
+}
