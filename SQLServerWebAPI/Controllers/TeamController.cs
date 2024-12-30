6c32e87586877ab1f8c49f8e94c32d1dd2d0f61c
Farah Alhaddad <Farah.alhaddad@hotmail.com>
2024-12-30 08:23:52 +0100

    Creating class TeamController with a  GET-method and a POST-method

---
+++ SQLServerWebAPI/Controllers/TeamController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLServerWebAPI.Models;
+
+
+namespace SQLServerWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Team
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Team>>> GetTeam()
+        {
+            return await _context.Teams.ToListAsync();
+        }
+
+        // POST: api/Team
+        [HttpPost]
+        public async Task<ActionResult<Team>> PostTeam(Team team)
+        {
+            _context.Teams.Add(team);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTeam), new { id = team.Id }, team);
+        }
+    }
+}
